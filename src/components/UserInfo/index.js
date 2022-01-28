ffe20bb4453fac49a3c4944ee6980951e35f5200
sravan9295 <saisravan9295@gmail.com>
2022-01-28 13:59:31 +0000

    success

---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,13 @@
+// Write your JS code here
+const UserInfo = () => (
+  <div className="userinfo">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+      alt="profile"
+    />
+    <h1>Wade Warren</h1>
+    <p>Software developer at UK</p>
+  </div>
+)
+
+export default UserInfo
